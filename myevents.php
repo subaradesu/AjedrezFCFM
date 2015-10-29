be48205ec23dc524a9cf6222c4c13c7f3c28fdba
subaradesu <kinsokujikoudeesu@gmail.com>
2015-10-28 23:10:19 -0300

    Barra de navegacion cambia por usuario (user/admin) Páginas "vacías" para las opciones de "mi cuenta" en la barra.

---
+++ myevents.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<?php session_start();?>
+<html lang="es">
+<head>
+	<!--Sets the page encoding-->
+	<meta charset="UTF-8">
+	<!-- page title -->
+	<title>Ajedrez Fcfm</title>
+	
+	<!-- Latest compiled and minified CSS (Bootstrap)-->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	
+	<!-- General Custom Style -->
+	<link href="default.css" rel="stylesheet">
+	
+	<!-- Ensures proper rendering on touch zooming -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<?php require_once 'utils.php';?>
+	<?php checkPermission(1);?>
+	
+</head>
+
+<body>
+
+<?php addNavBar();?>
+	
+<div class="container">
+	<div id="content">
+		<div class=page-header>
+			<h1>Mis Eventos:</h1>
+		</div>
+		<div>
+			<p>Acá puedes ver tus eventos.</p>
+		</div>
+	</div>
+</div>
+
+<?php addFooter();?>
+
+</body>
+</html>
--- navigation.php
+++ navigation.php
@@ -17,32 +17,41 @@
 				<li><a href="about.php">Historia</a>
 				<li><a href="links.php">Enlaces</a>
 				<li><a href="contact.php">Contacto</a>
+				<?php if(isLogged() && $_SESSION["permission"]==3):?>
+				<li><a href="#">Publicar</a>
+				<li><a href="#">Crear Evento</a>
+				<?php endif;?>
+				<li>
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
-				<?php
-				if(!isLogged()){
-					echo '
-						<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Ingresar</a></li>
-						<li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Registrarse</a></li>';
-				}
-				else{
+				<?php if(!isLogged()) : ?>
+					<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Ingresar</a></li>
+					<li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Registrarse</a></li>
+				<?php else : ?>
+					<?php
 					$s = $_SESSION["sex"]==2 ? 'a' : 'o';
-					echo '
-						<li><a>Bievenid'. $s . ', ' . $_SESSION["first_name"] . '.</a></li>
+					?>
+						<li><a>Bievenid<?php echo $s.', '.$_SESSION["first_name"];?></a></li>
 						<li class="dropdown">
 							<a class="dropdown-toggle" data-toggle="dropdown" href="news.php">Mi Cuenta
 							<span class="glyphicon glyphicon-cog"></span></a>
 						<ul class="dropdown-menu">
-							<li><a href="news.php">Mi Perfil</a></li>
-							<li class="disabled"><a href="#">Notificaciones(0)</a></li>
+							<li><a href="profile.php<?php echo '?id_user='.$_SESSION["username"]?>">Mi Perfil</a></li>
+							<li class="disabled"><a>Preferencias</a></li>
+							<li class="disabled"><a>Historial</a></li>
+							<?php if($_SESSION["permission"]==1 || $_SESSION["permission"]==3) : ?>
 							<li role="separator" class="divider"></li>
-							<li class="disabled"><a>Administrar</a></li>
-							<li class="disabled"><a>Publicar</a></li>
-						</ul>'.
-						//<li><a href="myaccount.php"><span class="glyphicon glyphicon-cog"></span> Mi Cuenta</a></li>
-						'<li><a href="logout.php"><span class="glyphicon glyphicon-off"></span> Cerrar Sesión</a></li>';
-				}
-				?>
+							<li><a href="myevents.php">Mis Eventos</a></li>
+							<?php endif;?>
+							<?php if($_SESSION["permission"]==3) : ?>
+							<li role="separator" class="divider"></li>
+							<li><a href="admin.php">Administrar</a></li>
+							<li><a href="publish.php">Publicar</a></li>
+							<li><a href="create_event.php">Crear Evento</a></li>
+							<?php endif;?>
+						</ul>
+						<li><a href="logout.php"><span class="glyphicon glyphicon-off"></span> Cerrar Sesión</a></li>
+				<?php endif; ?>
 			</ul>
 		</div>
 	</div>
