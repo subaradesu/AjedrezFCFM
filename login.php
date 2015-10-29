5206cd38caf19807b5e413456d9143df8ea78261
subaradesu <kinsokujikoudeesu@gmail.com>
2015-10-28 23:23:34 -0300

    Limpieza código login.

--- login.php
+++ login.php
@@ -27,34 +27,43 @@
 <body>
 <?php addNavBar();?>
 
-	
+
 <div class="container">
-	<?php 
-	if(!isLogged()){
-		if($logtry > 0){
-			echo '<div class="alert alert-warning">
-					<em>Nombre de Usuario y/o Contraseña incorrecto. Inténtalo de nuevo.</em>.
-				  </div>';
-		}
-		elseif($logtry > 2){
-			//TODO: mensaje de muchos reintentos.
-		}
-		include ("logpanel.html");
-	}
-	else{
-	$s = $_SESSION["sex"]==2 ? 'Bienvenida, ' : 'Bienvenido, ';
-	echo '
-	<div id="content">
-		<div class=page-header>
-			<h1>Ingreso Exitoso</h1>
+	<!-- 	Si no estoy loggeado muestro el formulario de inicio, si falle el login muestro notificacion -->
+	<?php if(!isLogged()) : ?>
+		<?php if ($logtry > 0) : ?>
+			<div class="alert alert-warning">
+				<em>Nombre de Usuario y/o Contraseña incorrecto. Inténtalo de nuevo.</em>.
+			</div>
+		<?php endif;?>
+		<?php if ($logtry > 2) : //TODO: mensaje de muchos reintentos.?>
+		<?php endif;?>
+		<div id="content">
+			<form class="form-signin" action="" method="POST">
+				<h2 class="form-signin-header"> Ingresar </h2>
+				
+				<label for="inputUsername" class="sr-only">Nombre de Usuario</label>
+				<input type="text" id="inputUsername" name="user" class="form-control" placeholder="Nombre de Usuario" required autofocus>
+				<label for="inputPassword" class="sr-only">Contraseña</label>
+				<input type="password" id="inputPassword" name="pass" class="form-control" placeholder="Contraseña" required>
+				<div class="checkbox">
+					<label>
+						<input type="checkbox" value="remember-me">No cerrar sesión 
+					</label>
+				</div>
+				<button class="btn btn-lg btn-primary btn-block" type="submit">Ingresar</button>
+				<p>¿Aún no tienes cuenta? <a href="register.php">Regístrate aquí.</a></p>
+			</form>
 		</div>
-	<div>
-	<div>
-		<p>'. $s . $_SESSION["first_name"].' '.$_SESSION["last_name"]. '. Disfruta tu estadía.</p>
-	</div>
-	';
-	}
-	?>
+	<!--	Si loguié con éxito muestro mensaje de bienvenida -->
+	<?php else : ?>
+		<?php $s = $_SESSION["sex"]==2 ? 'Bienvenida, ' : 'Bienvenido, ';?>
+		<div id="content">
+			<div class="alert alert-success">
+				<strong>Ingreso Exitoso!</strong> <?php echo $s. $_SESSION["first_name"].' '.$_SESSION["last_name"]. '. Disfruta tu estadía.'?>
+			</div>
+		</div>
+	<?php endif;?>
 </div>
 
 <?php addFooter();?>
